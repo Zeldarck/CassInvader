0d0f84ae3df254bc64f6db76ea902a2cd1408611
Etienne <fermivor@gmail.com>
2017-06-21 20:25:41 +0200

    pas grand chose, mais j'ai l'idée !

--- Assets/Script/Ennemy.cs
+++ Assets/Script/Ennemy.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Ennemy : MonoBehaviour {
 
+	[SerializeField]
+	private float enemySpeed = 1;
+
+	private Rigidbody _enemyRB;
+	private Vector2 _direction;
+
 	// Use this for initialization
 	void Start () {
-		
+		_enemyRB = gameObject.GetComponent<Rigidbody>();
+
+		_direction = new Vector2(1,0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		Vector3 directionToGo = new Vector3(0,0,0);
+		directionToGo.x = _direction.x * enemySpeed;
+		directionToGo.y = _direction.y * enemySpeed;
+
+		_enemyRB.velocity = directionToGo;
+	}
+
+	public void SetDirection(Vector2 dir){
+		_direction = dir;
+	}
+
+
+	// Destroy an ennemy when it collides with a projectile
+	public void Destroy(){
+
 	}
 }
