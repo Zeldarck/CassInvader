3b1bb33aec560e2896faa36458d8b8f46c9acb08
Zeldarck <zeldarck.martin@gmail.com>
2017-06-22 22:50:53 +0200

    projectile impulsion

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 1, 0);
 	}
 	
 	// Update is called once per frame
