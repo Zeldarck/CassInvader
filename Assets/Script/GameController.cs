04f84e20f0cf5c85898062f61f4ca935cdc124be
Etienne <fermivor@gmail.com>
2017-06-23 21:58:41 +0200

    game controller

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -7,6 +7,19 @@ public class GameController : MonoBehaviour {
 
     public static GameController INSTANCE;
 
+	private float _timeBetweenSpawn = 2;
+	private float _lastSpawnTime;
+
+	private float _timeBetweenLevel = 90;
+	private float _lastLevelTime;
+
+	private int _nbEnemyPerLevel;
+	private int _nbEnemyToSpawn;
+
+
+	[SerializeField]
+	private GameObject _enemy1;
+
 
     void Start () {
 		if(INSTANCE != null && INSTANCE != this)
@@ -17,10 +30,27 @@ public class GameController : MonoBehaviour {
         {
 			INSTANCE = this;
         }
+
+		// wait 5 sec before spawning begin
+		_lastSpawnTime = 3;
+		_nbEnemyPerLevel = 10;
+		_nbEnemyToSpawn = _nbEnemyPerLevel;
+
+		_lastLevelTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Time.time >= (_lastSpawnTime + _timeBetweenSpawn) && _nbEnemyToSpawn > 0){
+			Instantiate(_enemy1);
+			_lastSpawnTime = Time.time;
+			_nbEnemyToSpawn--;
+		}
+
+		if(Time.time >= (_lastLevelTime + _timeBetweenLevel)){
+			_nbEnemyToSpawn = _nbEnemyPerLevel;
+		}
 	}
 
 
